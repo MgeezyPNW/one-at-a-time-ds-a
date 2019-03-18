ceedc092a96309289b0a8063db56535b5f13af96
michaelageorge <mgeorge9492@gmail.com>
2019-03-18 07:47:33 -0700

    travis fix

--- array/nested-for-loop/nested-for-loop.test.js
+++ array/nested-for-loop/nested-for-loop.test.js
@@ -42,10 +42,10 @@ const salesData = (hours, data) => {
   hours.forEach((hour, idx) => {
     let hourlySales = {
       sales: `${data[idx]} cookies`,
-      time: hour
+      time: hour,
     };
     salesObjects.push(hourlySales);
-  })
+  });
   return salesObjects;
 };
 
@@ -57,16 +57,16 @@ Write a function named howManyTreats that will return the quantity of treats you
 const errands = [
   {
     store: 'Grocery store',
-    items: [{ name: 'Eggs', quantity: 12 }, { name: 'Milk', quantity: 1 }, { name: 'Apples', quantity: 3 }]
+    items: [{ name: 'Eggs', quantity: 12 }, { name: 'Milk', quantity: 1 }, { name: 'Apples', quantity: 3 }],
   },
   {
     store: 'Drug store',
-    items: [{ name: 'Toothpaste', quantity: 1 }, { name: 'Toothbrush', quantity: 3 }, { name: 'Mouthwash', quantity: 1 }]
+    items: [{ name: 'Toothpaste', quantity: 1 }, { name: 'Toothbrush', quantity: 3 }, { name: 'Mouthwash', quantity: 1 }],
   },
   {
     store: 'Pet store',
-    items: [{ name: 'Cans of food', quantity: 8 }, { name: 'Treats', quantity: 24 }, { name: 'Leash', quantity: 1 }]
-  }
+    items: [{ name: 'Cans of food', quantity: 8 }, { name: 'Treats', quantity: 24 }, { name: 'Leash', quantity: 1 }],
+  },
 ];
 
 const howManyTreats = (arr) => {
